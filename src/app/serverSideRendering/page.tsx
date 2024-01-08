d7f6f021870a06ae85a2d24b943b46dd4bf49953
Rishaba Priyan <rpcybermindworks@gmail.com>
2024-01-08 15:40:59 +0530

    wip setup

--- src/app/serverSideRendering/page.tsx
+++ src/app/serverSideRendering/page.tsx
@@ -2,18 +2,20 @@ import Content from "@components/Content";
 import { APP_ID, BASE_URL } from "@components/env";
 import axios from "axios";
 
-export const getAllUsers = async () => {
-  const users = await axios
-    .get(`${BASE_URL}/user?limit=10`, { headers: { "app-id": APP_ID } })
-    .then(({ data }) => {
-      return data;
-    });
-  return users;
-};
+// export const getAllUsers = async () => {
+//   const users = await axios
+//     .get(`${BASE_URL}/user?limit=10`, { headers: { "app-id": APP_ID } })
+//     .then(({ data }) => {
+//       return data;
+//     });
+//   return users;
+// };
 
 const ServerSideRenderingPage = async () => {
-  const users = await getAllUsers();
-  console.log("users", users);
-  return <Content content="This is server side rendered page" />;
+  // const users = await getAllUsers();
+  // console.log("users", users);
+  return (
+    <Content content="This is server side rendered page which is work in progress" />
+  );
 };
 export default ServerSideRenderingPage;
